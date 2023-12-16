05755606d67649db9367254578d9b5b5fbe67d50
thayrov <thayrovg@gmail.com>
2023-12-16 16:12:16 -0600

    💡 [ADD]: Implement initial database schema
    This commit introduces the initial SQL script for the CRM database schema, including entity creation for customers, employees, services, and related dimensions. Each table is designed to capture essential attributes such as identification, personal details, service descriptions, and transactional elements.
    
    The customer_dim table stores client information, whereas the employee_dim table contains employee records, both with fields for names, emails, and phones. Service-related tables like service_dim and service_category_dim hold details about the offerings and their categorization. The time_dim table is structured to log temporal data for transactions.
    
    Transactional facts are captured in interaction_fact, purchase_fact, purchase_detail_fact, detailing customer interactions, purchases, and individual purchase items. The customer_preferences_dim and customer_feedback_fact tables store customer preferences and feedback, respectively, which are crucial for CRM analytics.
    
    Foreign key relationships are established to maintain referential integrity, linking customers, employees, services, and time dimensions. The script enables comments for collaborative development and review.
    
    Complete entity-relationship diagrams and table descriptions, with abbreviations, full names, data types, and key types (foreign, primary, indices), are to be provided for comprehensive database documentation.

---
+++ Entregas/pre_entrega_1.sql
@@ -0,0 +1,106 @@
+-- Crear esquema para la base de datos
+CREATE SCHEMA CRM;
+USE CRM;
+
+-- Crear tabla customer_dim
+CREATE TABLE customer_dim (
+  customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phone VARCHAR(20) UNIQUE DEFAULT 'Not available'
+);
+
+-- Crear tabla employee_dim
+CREATE TABLE employee_dim (
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phone VARCHAR(20) UNIQUE DEFAULT 'Not available'
+);
+
+-- Crear tabla service_dim
+CREATE TABLE service_dim (
+  service_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  description TEXT NOT NULL,
+  price INT NOT NULL
+);
+
+-- Crear tabla service_category_dim
+CREATE TABLE service_category_dim (
+  service_category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL UNIQUE,
+  description TEXT NOT NULL
+);
+
+-- Crear tabla time_dim
+CREATE TABLE time_dim (
+  date_time DATETIME NOT NULL PRIMARY KEY,
+  year INT NOT NULL,
+  month INT NOT NULL,
+  day INT NOT NULL,
+  hour INT NOT NULL,
+  minute INT NOT NULL,
+  second INT NOT NULL
+);
+
+-- Crear tabla interaction_fact
+CREATE TABLE interaction_fact (
+  interaction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  employee_id INT NOT NULL,
+  service_id INT NOT NULL,
+  date_time DATETIME NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customer_dim(customer_id),
+  FOREIGN KEY (employee_id) REFERENCES employee_dim(employee_id),
+  FOREIGN KEY (service_id) REFERENCES service_dim(service_id),
+  FOREIGN KEY (date_time) REFERENCES time_dim(date_time)
+);
+
+-- Crear tabla purchase_fact
+CREATE TABLE purchase_fact (
+  purchase_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  date_time_purchase DATETIME NOT NULL,
+  total INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customer_dim(customer_id),
+  FOREIGN KEY (date_time_purchase) REFERENCES time_dim(date_time)
+);
+
+-- Crear tabla purchase_detail_fact
+CREATE TABLE purchase_detail_fact (
+  id_purchase_detail INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  purchase_id INT NOT NULL,
+  service_id INT NOT NULL,
+  purchase_price INT NOT NULL,
+  FOREIGN KEY (purchase_id) REFERENCES purchase_fact(purchase_id),
+  FOREIGN KEY (service_id) REFERENCES service_dim(service_id)
+);
+
+-- Crear tabla customer_preferences_dim
+CREATE TABLE customer_preferences_dim (
+  preference_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  service_category_id INT NOT NULL,
+  preferred_employee_id INT NOT NULL,
+  notes TEXT DEFAULT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customer_dim(customer_id),
+  FOREIGN KEY (service_category_id) REFERENCES service_category_dim(service_category_id),
+  FOREIGN KEY (preferred_employee_id) REFERENCES employee_dim(employee_id)
+);
+
+-- Crear tabla customer_feedback_fact
+CREATE TABLE customer_feedback_fact (
+  feedback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  service_id INT NOT NULL,
+  rating INT NOT NULL,
+  comment TEXT,
+  date_time DATETIME NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customer_dim(customer_id),
+  FOREIGN KEY (service_id) REFERENCES service_dim(service_id),
+  FOREIGN KEY (date_time) REFERENCES time_dim(date_time)
+);
